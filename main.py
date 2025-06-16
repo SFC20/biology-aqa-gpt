18821f942a51a3d928142113e010386ffcb315c1
Sophie FC <143188401+SFC20@users.noreply.github.com>
2025-06-16 12:13:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,6 @@ def get_response():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))  # Render assigns PORT dynamically
+    app.run(host="0.0.0.0", port=port)
 
