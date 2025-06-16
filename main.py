e61a76f27cb8f6feb1588e521609d2abed90c8dc
Sophie FC <143188401+SFC20@users.noreply.github.com>
2025-06-16 12:09:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,5 @@ def get_response():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("HTTP_PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
